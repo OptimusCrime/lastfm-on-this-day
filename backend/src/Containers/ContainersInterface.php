6bae8d52a7590f538fe4083f9e4614d50227eec3
OptimusCrime <thomas.gautvedt@gmail.com>
2025-10-09 00:09:18 +0200

    chore: remove backend in php + docker files

--- backend/src/Containers/ContainersInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Optimuscrime\Lastfm\Containers;
-
-use Psr\Container\ContainerInterface;
-
-interface ContainersInterface
-{
-    public static function load(ContainerInterface $container): void;
-}
