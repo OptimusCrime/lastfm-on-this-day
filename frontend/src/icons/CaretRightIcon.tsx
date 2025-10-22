b0f40a4e3e6ba81c6ac612295471c3e22c0f6e2c
OptimusCrime <thomas.gautvedt@gmail.com>
2025-10-22 07:05:52 +0200

    fix: safari is being dumb

--- frontend/src/icons/CaretRightIcon.tsx
+++ frontend/src/icons/CaretRightIcon.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 export const CaretRightIcon = () => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" className="fill-neutral-content shrink-0">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 256 256"
+      className="fill-current shrink-0"
+      width="100%"
+      height="100%"
+    >
       <rect width="256" height="256" fill="none" />
       <circle
         cx="128"
